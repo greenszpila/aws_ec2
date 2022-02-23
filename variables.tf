8d47fa18919c27091ddd038e58c2f61a11fc2c4e
Krzysztof <kspikowski@gmail.com>
2022-02-23 20:54:58 +0000

    added comments

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 //variables.tf
 
-// please modify region, key_pair_name accordingly , optionally instance_size
+# please modify region and key_pair_name accordingly , 
+# optionally instance_size e.g. t2.micro is not available in the eu-north-1 region
 
 variable "aws_region" { default = "us-east-2" } # Ohio
 #variable "aws_region" { default = "eu-west-1" } # Dublin
 
+# modify this to match with your .pem key location, this variable is being used in the `ssh connect` output
 variable "private_key_location" {
   type = string
   default = "~/coding/kriss.pem"
 }
+
 variable "ami_key_pair_name" { default = "kriss" }
 #variable "ami_key_pair_name" { default = "kriss-eu" }
 
